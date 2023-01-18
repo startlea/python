1eca551e2f1cc2774e60a8bef090b02fc103f275
startlea <eos1984kowalska@gmail.com>
2023-01-18 18:01:44 +0100

    rollercoster

---
+++ rolercoster.py
@@ -0,0 +1,11 @@
+#Description
+print("Welcome to the rollercoster!\n")
+
+#Variables
+height = int(input("Write your height in cm: \n"))
+
+#Result
+if height > 120:
+    print("You can take a ride!\n")
+else:
+    ("You don't have enough height to take a ride. Come back another time.\n")
