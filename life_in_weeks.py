745d5c0dc788faf569b997624caeb8fb008134a7
startlea <eos1984kowalska@gmail.com>
2023-01-18 08:48:35 +0100

    New file life_in_weeks

---
+++ life_in_weeks.py
@@ -0,0 +1,6 @@
+age=int(input("How old are you? "))
+age_day=int(age*365)
+age_weeks=int(age*52)
+age_month=int(age*12)
+
+print(f"You are {age_day} days, {age_weeks} weeks and {age_month} months old.\n")
