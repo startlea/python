cad96179b6fcab375527e698ff41d6297d3adde3
startlea <eos1984kowalska@gmail.com>
2023-01-19 10:54:28 +0100

    BMI_calculator_nested

---
+++ BMI_calculator_nested.py
@@ -0,0 +1,20 @@
+#Description
+print("This is BMI calculator")
+
+#Variables
+weight = int(input("Type in your weight: \n"))
+height = float(input("Type in your height: \n"))
+
+#Result
+BMI = round(weight / height ** 2)
+
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}. You are Underweight.\n")
+elif BMI < 25:
+    print(f"Your BMI is {BMI}. You have normal weight.\n")
+elif BMI < 30:
+    print(f"Your BMI is {BMI}. You are overweight.\n")
+elif BMI < 35:
+    print(f"Your BMI is {BMI}. You are obese.\n")
+elif BMI > 35:
+    print(f"Your BMI is {BMI}. You are clinically obeese.\n")
