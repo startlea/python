1fba670fc460ff537d68499e307381495da8ac06
startlea <eos1984kowalska@gmail.com>
2023-01-19 10:02:29 +0100

    Odd_or_even_number

---
+++ odd_or_even.py
@@ -0,0 +1,11 @@
+#Description
+print("This program check if the number is odd or even.\n")
+
+#Variables
+number = int(input("What number you wish to check?\n"))
+
+#Result
+if number % 2 == 0:
+    print("This is an even number.\n")
+else:
+    print("This is an odd number.\n")
